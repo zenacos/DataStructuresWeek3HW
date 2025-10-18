3aa9116909824d07ce92684a0887e7df73b03e4c
Zeynep Nazlı Coşkun <coskunnazli04@gmail.com>
2025-10-18 20:33:48 +0300

    Add files via upload

---
+++ MinHeapInterface.java
@@ -0,0 +1,8 @@
+public interface MinHeapInterface<T extends Comparable<? super T>> {
+    public void add(T newEntry);
+    public T removeMin();
+    public T getMin();
+    public boolean isEmpty();
+    public int getSize();
+    public void clear();
+}
