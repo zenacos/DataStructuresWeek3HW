6585ff0d726f55d373602d19eb128de3c2a2bb06
Zeynep Nazlı Coşkun <coskunnazli04@gmail.com>
2025-10-20 16:10:36 +0300

    Add files via upload

---
+++ Week3/src/exercise1/Exercise1_Test.java
@@ -0,0 +1,25 @@
+package exercise1;
+
+public class Exercise1_Test {
+
+    public static void main(String[] args) {
+        Integer[] A = {14, 20, 2, 15, 10, 21};
+
+        MaxHeapInterface<Integer> maxHeap = new MaxHeap<>();
+        for (int i = 0; i < A.length; i++) {
+            maxHeap.add(A[i]);
+        }
+
+        if (maxHeap.isEmpty())
+            System.out.println("The heap is empty - INCORRECT");
+        else
+            System.out.println("The heap is not empty; it contains " +
+                    maxHeap.getSize() + " entries.");
+
+        System.out.println("The largest entry is " + maxHeap.getMax());
+
+        System.out.println("\n\nRemoving entries in descending order:");
+        while (!maxHeap.isEmpty())
+            System.out.println("Removing " + maxHeap.removeMax());
+    }
+}
